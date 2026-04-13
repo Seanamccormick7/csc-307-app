83e160fe48abec20953330bfd69d9341566f3d3b
Sean <Seanamccormick7@gmail.com>
2026-04-13 07:39:25 -0700

    add POST /users route

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -59,6 +59,17 @@ app.get("/users", (req, res) => {
   }
 });
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"];
   let result = findUserById(id);
