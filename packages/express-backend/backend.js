8cc021454ce91d0ada2e87ed81f4abc1688101be
Sean <Seanamccormick7@gmail.com>
2026-04-27 16:12:35 -0700

    connect backend to MongoDB with mongoose, add model and service layer, update frontend to use _id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,17 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userService from "./services/user-service.js";
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users")
+  .catch((error) => console.log(error));
 
 const app = express();
 const port = 8000;
@@ -7,106 +19,65 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspiring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
-
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-  if (name != undefined && job != undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  } else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
-});
-
-function generateId() {
-  return Math.random().toString(36).substring(2, 9);
-}
-
-const addUser = (user) => {
-  const newUser = { id: generateId(), ...user };
-  users["users_list"].push(newUser);
-  return newUser;
-};
-
-app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  const newUser = addUser(userToAdd);
-  res.status(201).send(newUser);
-});
-
-app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"];
-  const index = users["users_list"].findIndex((user) => user["id"] === id);
-  if (index === -1) {
-    res.status(404).send("Resource not found.");
-  } else {
-    users["users_list"].splice(index, 1);
-    res.status(204).send();
-  }
+  userService.getUsers(name, job)
+    .then((users) => {
+      res.send({ users_list: users });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Internal server error.");
+    });
 });
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"];
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  userService.findUserById(id)
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Internal server error.");
+    });
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  userService.addUser(userToAdd)
+    .then((newUser) => {
+      res.status(201).send(newUser);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Internal server error.");
+    });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  userService.deleteUserById(id)
+    .then((deleted) => {
+      if (deleted === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.status(204).send();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Internal server error.");
+    });
 });
 
 app.listen(port, () => {
